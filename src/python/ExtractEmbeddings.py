573ce9b2e022dacadac9429f933faf356e514e17
Mahdi Islam <islam.mahdi@gmail.com>
2026-09-13 17:49:13 +0200

    Fix folder structure

--- src/python/ExtractEmbeddings.py
+++ src/python/ExtractEmbeddings.py
@@ -11,7 +11,7 @@ logging.set_verbosity_error()
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = BertForSequenceClassification.from_pretrained("prajjwal1/bert-tiny")
-trained = torch.load('../notebooks/SST-2-BERT-tiny.bin', map_location=torch.device('cpu'))
+trained = torch.load('./notebooks/SST-2-BERT-tiny.bin', map_location=torch.device('cpu'))
 model.load_state_dict(trained, strict=False)
 model.eval()
 
@@ -29,6 +29,6 @@ x = model.bert.embeddings(tokens_tensor, torch.tensor([[1] * len(tokenized_text)
 
 #Export x
 for i in range(len(x[0])):
-    np.savetxt('../src/tmp_embeddings/input_{}.txt'.format(i), x[0][i].detach(), delimiter=',')
+    np.savetxt('./src/tmp_embeddings/input_{}.txt'.format(i), x[0][i].detach(), delimiter=',')
 
 #print("{} embeddings correctly saved in \"tmp_embeddings\" folder".format(len(x[0])))
--- src/python/PlainCircuit.py
+++ src/python/PlainCircuit.py
@@ -8,7 +8,7 @@ logging.set_verbosity_error()
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 model = BertForSequenceClassification.from_pretrained("prajjwal1/bert-tiny")
-trained = torch.load('../notebooks/SST-2-BERT-tiny.bin', map_location=torch.device('cpu'))
+trained = torch.load('./notebooks/SST-2-BERT-tiny.bin', map_location=torch.device('cpu'))
 model.load_state_dict(trained, strict=False)
 model.eval()
 
